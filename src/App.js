da78bb6808e77f6b061404ee01f102eee711c45f
LinearMilk <witold.henszel@gmail.com>
2017-11-03 17:56:22 +0100

    Dinner menu
    Dinner menu react component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import DishList from './Components/DishList/DishList';
 import StaffList from './Components/StaffList/StaffList';
 import LunchDishList from './Components/LunchDishList/LunchDishList';
+import DinnerDishList from './Components/DinnerDishList/DinnerDishList';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <div className="col-md-6">
           <LunchDishList />
+          <DinnerDishList />
         </div>
 
         <div className="col-md-5">
