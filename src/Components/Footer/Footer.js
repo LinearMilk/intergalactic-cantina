0759102b2abcaf35682430f10e6a526db46ecd97
LinearMilk <witold.henszel@gmail.com>
2017-11-07 20:09:47 +0100

    Fixed images
    Fixed images not showing in production and split app into more
    components.

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+render(){
+  return (
+    <footer>
+      <div class="container">
+        <h2>Where to Find Us</h2>
+        <p>Antennae Galaxies (NGC 4038/NGC 4039), Corvus constellation.</p>
+        <div class="row"><img src="img/2xcluster.jpg"/></div>
+      </div>
+    </footer>
+  )
+}
+}
+
+export default Footer;
