69af9600b2c6df16d78070d635fa9e0640adda58
LinearMilk <witold.henszel@gmail.com>
2017-11-15 11:14:39 +0100

    Routes added

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Main.css';
+import StaffList from '../StaffList/StaffList';
+import LunchDishList from '../LunchDishList/LunchDishList';
+import DinnerDishList from '../DinnerDishList/DinnerDishList';
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <LunchDishList />
+            <DinnerDishList />
+          </div>
+          <div className="col-md-5">
+            <StaffList />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
