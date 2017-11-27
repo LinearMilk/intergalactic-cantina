d93bd0375c253e28390fde2c564ec1b63a2759a3
LinearMilk <witold.henszel@gmail.com>
2017-11-27 22:34:49 +0100

    severak beverages added in multiple categories

--- src/Components/DishList/beverages.js
+++ src/Components/DishList/beverages.js
@@ -3,28 +3,96 @@ let beverages = [
   subcategory: 'wines',
   content: [
   {
-    name: 'liquid lunch potatoes',
+    name: 'Klingon Bloodwine',
     price: '$7',
-    description: 'Your old regular spuds. Microwaved to achieved perfect balance between mushy and stone-hard.',
-    tags: ['vegan', 'non-sentient']
+    description: 'Perhaps today is a good day to die.',
+    tags: ['klingon', 'alcoholic']
   },
   {
-    name: 'blended lunch lentils',
+    name: 'chateau picard',
     price: '$5',
-    description: 'Dry split red lentils. Be prepared to do a lot of chewing.',
-    tags: ['vegan', 'non-sentient']
+    description: 'Crimson colour, delicately smoky and spiced aroma, a sophisticated and balanced wine: a vintage of great style',
+    tags: ['terran', 'alcoholic']
   }
 ]},
 {
   subcategory: 'beers',
   content: [
   {
-    name: 'lunch chicken smoothie',
-    price: '$20',
-    description: 'Space chicken. No one really knows what is is, but it tastes fantasic.',
-    tags: ['vegan']
+    name: 'benderbrau',
+    price: '$6',
+    description: 'Cold-fusion steam beer.',
+    tags: ['alcoholic']
+  },
+  {
+    name: 'butterbeer',
+    price: '$7',
+    description: 'Enjoy a foaming mug of this popular wizarding beverage.',
+    tags: ['alcoholic']
   }
-]}
+]},
+{
+  subcategory: 'spirits',
+  content: [
+  {
+    name: 'Acamarian brandy',
+    price: '$20',
+    description: 'An amber-colored alcoholic beverage from Acamar III',
+    tags: ['alcoholic']
+  },
+  {
+    name: 'Synthehol',
+    price: '25',
+    description: 'All the taste and smell, none of the effects.',
+    tags: ['ferengi'],
+  }
+]},
+{
+  subcategory: 'cocktails',
+  content: [
+  {
+    name: 'Pan Galactic Gargle Blaster',
+    price: '$500',
+    description: 'Best drink in existence. You should NEVER have more than two.',
+    tags: ['alcoholic']
+  },
+  {
+    name: "Finagl's Folly",
+    price: '$50',
+    description: 'Known from here to Orion.',
+    tags: ['alcoholic']
+  }
+
+]},
+{
+  subcategory: 'hot drinks',
+  content: [
+  {
+    name: 'earl gray tea. hot.',
+    price: '$2',
+    description: 'To boldly go where no one has gone before.',
+    tags: ['terran']
+  },
+  {
+    name: 'Overpriced coffee',
+    price: '$37',
+    description: 'AWESOME!',
+    tags: ['red-mug']
+  },
+  {
+    name: 'Nuka-cola',
+    price: '$7',
+    description: 'Warm, irradiated, and flat.',
+    tags: ['radioactive']
+  }
+
+
+
+
+
+]},
+
+
 ];
 
 export default beverages;
