26d9df4858bdf7f6fd474a72626437dba620d8fe
Luara Neumann <luaralulu@gmail.com>
2017-12-01 17:49:17 +0100

    New styling for Staff

--- src/Components/StaffList/StaffList.js
+++ src/Components/StaffList/StaffList.js
@@ -8,14 +8,14 @@ class StaffList extends React.Component {
   render (){
     return (
       <div className='staff-list media'>
-        <h1>Our Staff</h1>
+        <h1>We&#39;re Here for You</h1>
         {(theStaff.length) ? theStaff.map((staff, i) =>
-          <div key={i} className="col-sm-6 col-lg-4">
+          <div key={i} className="col-sm-6">
             <Staff key={i}
                    name={staff.name}
                    thumbnail={staff.thumbnail}
                    title={staff.title}
-                   description={staff.description} />
+                   thumbnailalign={((i%2)===0)?'left':'right'}/>
           </div>
         ) : <p>No One working here atm.</p>
         }
