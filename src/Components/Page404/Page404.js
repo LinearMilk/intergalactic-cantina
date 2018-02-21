c4e3c8457d5578ba10d480362cd0eb3f20b62173
Luara Neumann <luaralulu@gmail.com>
2018-02-21 07:01:01 +0100

    Changing the font for the text

--- src/Components/Page404/Page404.js
+++ src/Components/Page404/Page404.js
@@ -6,7 +6,7 @@ class Page404 extends Component {
     return (
       <div className="container">
         <div className='error-header'>
-          <h1>Oops, you ended up at the wrong spacetime coordinates. (#404) </h1>
+          <p>Oops, you ended up at the wrong spacetime coordinates. (#404) </p>
           <img className="image-404" src="./img/404/marvin_42.png" alt="marvin the paranoid android"/>
           <div className="clearfix"></div>
         </div>
